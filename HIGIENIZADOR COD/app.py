797b9b2e66ecaeb1ffb2f4dbcc35762ae26dee75
Henrique Meirelles <141285929+hnrqem@users.noreply.github.com>
2026-04-23 16:02:21 -0300

    Update app.py

--- HIGIENIZADOR COD/app.py
+++ HIGIENIZADOR COD/app.py
@@ -75,8 +75,7 @@ def aprender():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
